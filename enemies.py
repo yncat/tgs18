ea4b7e4ee3e575f738a2c57772ef73b470754681
nyanchan <personal@nyanchangames.com>
2019-06-28 18:53:19 +0900

    Wrapped openAL class so that I can add another audio backend

--- enemies.py
+++ enemies.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # TGS19 enemies
 # Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
-import openal
 import random
 import math
+import vrsound
 import window
 
 INITIAL_DISTANCE=2.0
@@ -14,9 +14,8 @@ class Mosquito(object):
 		self.world=world
 		self.degrees=random.randint(0,359)
 		self.distance=INITIAL_DISTANCE
-		self.flying_sound=openal.oalOpen("fx/mosquito.ogg")
-		self.flying_sound.set_looping(True)
-		self.flying_sound.set_gain(6.0)
+		self.flying_sound=vrsound.load("fx/wings1.ogg")
+		self.flying_sound.setLooping(True)
 		self.updatePosition()
 		self.turn_speed=-5 if random.randint(0,1)==0 else 5
 		self.approach_facter=0
@@ -27,7 +26,7 @@ class Mosquito(object):
 		rad=math.radians(self.degrees)
 		self.x=math.cos(rad)*INITIAL_DISTANCE
 		self.z=math.sin(rad)*self.distance
-		self.flying_sound.set_position((self.x,0,self.z))
+		self.flying_sound.setPosition((self.x,0,self.z))
 
 	def frameUpdate(self):
 		if self.timer.elapsed<50: return
---
+++ vrsound.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# TGS19 3D sound wrapper for multiple audio backends
+# Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
+import openal
+
+def load(path): return OpenAlSource(path)
+
+class OpenAlSource(object):
+	def __init__(self,path):
+		self.handle=openal.oalOpen(path)
+
+	def play(self):
+		self.handle.play()
+
+	def stop(self):
+		self.handle.stop()
+
+	def setLooping(self,lp):
+		self.handle.set_looping(lp)
+
+	def setPosition(self,pos):
+		self.handle.set_position(pos)
